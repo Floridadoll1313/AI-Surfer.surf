64d5686cb5b074c9b690b7a405ca387409b566ca
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-15 18:46:00 -0400

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL=https://yzcugymxyldvlknmqjzx.supabase.co;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY=0ad95db23ca9c2eeb0c3a970f8bfc0de;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
